4b7571e2fbe6d422e479129eabad392330a6eb98
Vijay Patel <vijaypatel3207@gmail.com>
2025-01-24 23:51:23 +0530

    employee rendering is done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
 async function fetchData(){
     const data = await fetch('./src/dummy.json')
-    const res = await data.json();
-    console.log(res);
+    const employee = await data.json();
+    console.log(employee);
+
+    let selectedEmployeesId = employee[0].id;
+    let selectedEmployee = employee[0];
+
+    const employeeList = document.querySelector('.employees__name__list');
+    const employeeInfo = document.querySelector('.employees__single__list');
+
+
+    const renderEmployee = () => {
+        employeeList.innerHTML = '';
+        employee.forEach(emp => {
+            const employeeItem = document.createElement('span');
+            employeeItem.classList.add('employees__name__item');
+
+
+            if(parseInt(selectedEmployeesId, 10) === emp.id){
+                employeeItem.classList.add('selected');
+                selectedEmployee = emp;
+            }
+
+            employeeItem.setAttribute('id', emp.id);
+            employeeItem.innerHTML = `${emp.firstName} ${emp.lastName} <i class="employeeDelete">X</>`;
+
+            employeeList.append(employeeItem);
+        });   
+    }
+
+    renderEmployee();
 }
 
 fetchData();
